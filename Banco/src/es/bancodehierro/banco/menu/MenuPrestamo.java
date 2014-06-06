ed2c17177b67074340902811f1e7cc931e5b9b07
mvallespir <vallespirmiquel@gmail.com>
2014-06-06 20:43:46 +0200

    Creacion menu prestamo

---
+++ Banco/src/es/bancodehierro/banco/menu/MenuPrestamo.java
@@ -0,0 +1,15 @@
+package es.bancodehierro.banco.menu;
+
+/**
+ *
+ * @author Miquel Vallespir, Rafel Sastre, Pau Riera, Jaume Mayol, Tomeu Moranta
+ */
+public class MenuPrestamo {
+    
+    public void muestraMenu(){
+        
+        
+        
+    }
+    
+}
