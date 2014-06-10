d5464d0191f293e811d14acaa6c2ff9a2568e530
guillem <tukav73@gmail.com>
2014-06-10 20:54:41 +0200

    Menu principal actualizado, ARREGLAD LAS TARJETAS!. Grupo Guillems, François i Pedro.

--- Banco/src/es/bancodehierro/banco/menu/Principal.java
+++ Banco/src/es/bancodehierro/banco/menu/Principal.java
@@ -18,32 +18,40 @@ public abstract class Principal {
     private static final int MENU_PRINCIPAL_PRESTAMO = 69001;
     private static final int MENU_PRINCIPAL_TARJETA = 69002;
     private static final int MENU_PRINCIPAL_SUCURSAL = 69003;
-
+    private static final int MENU_PRINCIPAL_SALIR = 69004;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         System.out.println("BIENVENIDO AL BANCO DE HIERRO");
         String dni = GestionaMenu.llegirCadena("inserta tu DNI");
-        String[] opciones = {"Cunta corriente", "Opcion2", "Opcion3"};
-        int op = gestionarMenu("Menu Principal", opciones, "Elige una opción", MENU_PRONCIPAL_PREFIX);
-        switch (op) {
-            case MENU_PRINCIPAL_CC:
-                //MenuCuentaCorriente.menuCC;
-                break;
+        String[] opciones = {"Cunta corriente", "Prestamo", "Tarjeta","Sucursal","Salir"};
+        boolean menu = true;
+        do {
+            int op = gestionarMenu("Menu Principal", opciones, "Elige una opción", MENU_PRONCIPAL_PREFIX);
+            switch (op) {
+                case MENU_PRINCIPAL_CC:
+                    //MenuCuentaCorriente.menuCC;
+                    break;
 
-            case MENU_PRINCIPAL_PRESTAMO:
-                //Aqui debe llamar al metodo principal del menu de prestamo
-                break;
+                case MENU_PRINCIPAL_PRESTAMO:
+                    //Aqui debe llamar al metodo principal del menu de prestamo
+                    break;
 
-            case MENU_PRINCIPAL_TARJETA:
-                //Aqui debe llamar al metodo principal del menu de tarjeta
-                break;
-            case MENU_PRINCIPAL_SUCURSAL:
-                //Aqui debe llamar al metodo principal del menu de sucursal
-                MenuSucursal.menu();
-                break;
-        }
+                case MENU_PRINCIPAL_TARJETA:
+                    //Aqui debe llamar al metodo principal del menu de tarjeta
+                    break;
+                case MENU_PRINCIPAL_SUCURSAL:
+                    //Aqui debe llamar al metodo principal del menu de sucursal
+                    MenuSucursal.menu();
+                    break;
+                case MENU_PRINCIPAL_SALIR:
+                    menu=false;
+                    System.out.println("Sortint...");
+                    break;
+            }
+        } while (menu);
     }
 
 }
