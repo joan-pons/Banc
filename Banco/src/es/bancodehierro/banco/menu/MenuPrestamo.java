90bbe1d01d36bcab914476e79b386a4b096b2285
RuralGalaxy <bitbucketrural88>
2014-06-10 20:55:26 +0200

    insertarPrestamo

--- Banco/src/es/bancodehierro/banco/menu/MenuPrestamo.java
+++ Banco/src/es/bancodehierro/banco/menu/MenuPrestamo.java
@@ -1,5 +1,11 @@
 package es.bancodehierro.banco.menu;
 
+import es.bancodehierro.banco.prestamo.Prestamo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Miquel Vallespir, Rafel Sastre, Pau Riera, Jaume Mayol, Tomeu Moranta
@@ -16,6 +22,25 @@ public class MenuPrestamo {
             case 1:
 
                 break;
+        }
+    }
+
+    /**
+     * PENDIENTE DE PASAR A BANCO Y INDICAR BASE DE DATOS.
+     *
+     * @author Rafel Sastre.
+     * @param presta
+     */
+    public void insertarPrestamo(Prestamo presta) {
+        String url = "jdbc:mysql://localhost:3306/biblioteca?user=root&password=";
+        Connection conexio;
+        try {
+            conexio = DriverManager.getConnection(url);
+            Statement st = conexio.createStatement();
+            int filesAfectades = st.executeUpdate(presta.inserir());
+            System.out.println(filesAfectades + ", files afectades.");
+        } catch (SQLException ex) {
+            System.out.println("ERROR: " + ex.getErrorCode() + ", " + ex.getLocalizedMessage());
         }
 
     }
