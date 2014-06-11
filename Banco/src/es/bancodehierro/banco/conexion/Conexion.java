2c5aec66195c1300b4cb633685f5e9226af624dc
antonio <antonio@192.168.7.182>
2014-06-11 18:16:22 +0200

    Conexion actualizada2

--- Banco/src/es/bancodehierro/banco/conexion/Conexion.java
+++ Banco/src/es/bancodehierro/banco/conexion/Conexion.java
@@ -1,9 +1,10 @@
+package es.bancodehierro.banco.conexion;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package banc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
