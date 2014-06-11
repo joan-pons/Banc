17f17506494fc7a2a50597a68c1e13edb421dd4a
Pau <Pau@192.168.7.171>
2014-06-11 16:08:45 +0200

    Afegit plugin Oracle. Eliminat plugin MYSQL.

---
+++ Banco/src/es/bancodehierro/banco/central/ConexionBanco.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package es.bancodehierro.banco.central;
+
+/**
+ *
+ * @author Pau
+ */
+public class ConexionBanco {
+    
+    public static void conexion(){
+        
+    }
+}
