1b016cc5f35ab634ad319c793c0aeb916bac10d8
zadioke <bernadi.beltran@gmail.com>
2014-06-11 19:20:02 +0200

    ajustando metodos con la clase conexionnn

--- Banco/src/es/bancodehierro/banco/tarjeta/Credito.java
+++ Banco/src/es/bancodehierro/banco/tarjeta/Credito.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package es.bancodehierro.banco.tarjeta;
+package es.bancodehierro.banco.Tarjeta;
 
 import es.bancodehierro.banco.cc.CuentaCorriente;
 import es.bancodehierro.banco.persona.Cliente;
