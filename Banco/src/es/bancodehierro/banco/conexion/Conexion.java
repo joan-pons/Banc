bf10a3ebb91a5bbbc8765bf244f6a4374bb95c44
antonio <antonio@192.168.7.182>
2014-06-11 16:39:24 +0200

    Actualizado conexion (metodo desconectar)

--- Banco/src/es/bancodehierro/banco/conexion/Conexion.java
+++ Banco/src/es/bancodehierro/banco/conexion/Conexion.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package es.bancodegierro.banco.conexion;
+package es.bancodehierro.banco.conexion;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
