32b9b7fc4708d3136ba3cf7da35553f7d9bacd06
Marc <Marc@MARCPORTATIL>
2014-06-12 18:59:03 +0200

    Commit

--- Banco/src/es/bancodehierro/banco/tarjeta/Tarjeta.java
+++ Banco/src/es/bancodehierro/banco/tarjeta/Tarjeta.java
@@ -1,117 +1,94 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package es.bancodehierro.banco.tarjeta;
 
 import es.bancodehierro.banco.conexion.Conexion;
-import es.bancodehierro.banco.cc.CuentaCorriente;
-import es.bancodehierro.banco.persona.Cliente;
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.text.SimpleDateFormat;
+import oracle.sql.TIMESTAMP;
 
-/**
- *
- * @author Marc
- */
-public abstract class  Tarjeta {
+public abstract class Tarjeta {
 
-    
-    int codigoTarjeta;
-    private int codigoTitular;
-    private int codigoCuentaCorriente;
-    private  String tipo;
+    protected String codigoTarjeta;
+    private String codigoTitular;
+    private String codigoCuentaCorriente;
+    private int codigoSucursal;
+    private String tipo;
+    private String fechaTarjeta;
 
-    public Tarjeta(int codigoTarjeta, int codigoTitular, int codigoCuentaCorriente, String tipo) {
+    public Tarjeta(String codigoTarjeta, String codigoTitular, String codigoCuentaCorriente, int codigoSucursal, String tipo, String fechaTarjeta) {
         this.codigoTarjeta = codigoTarjeta;
         this.codigoTitular = codigoTitular;
         this.codigoCuentaCorriente = codigoCuentaCorriente;
+        this.codigoSucursal = codigoSucursal;
         this.tipo = tipo;
-    
-        try {
-            Connection conexion = Conexion.conectar();
-            Statement consulta = conexion.createStatement();
-            int filesAfectades = consulta.executeUpdate("insert into ");
-
-        } catch (SQLException ex) {
-            Logger.getLogger(Tarjeta.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    
+        this.fechaTarjeta = fechaTarjeta;
     }
- 
     
-    /**
-     * Este metodo es las sobrecarga de el constructor de Tarjeta, 
-     * devuelve la tarjeta que conicide con el codigo de tarjeta que esta
-     * guardada en la base de datos.
-     * @param codigoTarjeta 
-     */
-   public Tarjeta(int codigoTarjeta){
-     
-       ResultSet resultat;
-       String tipoT=null;
-       int codTitular = 0;
-       int codCuentaCorriente = 0;
+    public Tarjeta(String codigoTarjeta, String tipo){
+        this.codigoTarjeta = codigoTarjeta;
         try {
-            Connection conexion = Conexion.conectar();
-             Statement consulta = conexion.createStatement();
-            resultat = consulta.executeQuery("select * from Tarjeta where codigoTarjeta ="+ codigoTarjeta);
-              while(resultat.next()){
-                codTitular = resultat.getInt("DNI_CLIENTE_TARJETA");
-                codCuentaCorriente = resultat.getInt("NUMERO_CC_TARJETA");
-                tipoT = resultat.getString("TIPO_TARJETA");
-              }
-        if(resultat!=null) resultat.close();
-        
-        }
-        catch (SQLException ex) {
-            Logger.getLogger(Tarjeta.class.getName()).log(Level.SEVERE, null, ex);
-        }
-          
-        this.codigoTarjeta = codigoTarjeta;
-        this.codigoTitular = codTitular;
-        this.codigoCuentaCorriente = codCuentaCorriente;
-        this.tipo = tipo;
-        
-   }
+            ResultSet select = Conexion.conectar().createStatement().executeQuery("SELECT * FROM v_tarjeta_credito WHERE codigo_tarjeta = '" + codigoTarjeta + "'");
+            while (select.next()) {
+                this.codigoTitular = select.getString(2);
+                this.codigoCuentaCorriente = select.getString(3);
+                this.codigoSucursal = select.getInt(4);
+                this.tipo = tipo;
+                this.fechaTarjeta = new SimpleDateFormat("yyyy/MM/dd").format(select.getTimestamp(5));
+            }
+        } catch (SQLException e) {
 
-    public void setCodigoTarjeta(int codigoTarjeta) {
+        }
+    }
+
+    @Override
+    public abstract String toString();
+
+    public String getFechaTarjeta() {
+        return fechaTarjeta;
+    }
+
+    public void setFechaTarjeta(String fechaTarjeta) {
+        this.fechaTarjeta = fechaTarjeta;
+    }
+
+    public String getCodigoTarjeta() {
+        return codigoTarjeta;
+    }
+
+    public void setCodigoTarjeta(String codigoTarjeta) {
         this.codigoTarjeta = codigoTarjeta;
+    }
+
+    public String getCodigoTitular() {
+        return codigoTitular;
+    }
+
+    public void setCodigoTitular(String codigoTitular) {
+        this.codigoTitular = codigoTitular;
+    }
+
+    public String getCodigoCuentaCorriente() {
+        return codigoCuentaCorriente;
+    }
+
+    public void setCodigoCuentaCorriente(String codigoCuentaCorriente) {
+        this.codigoCuentaCorriente = codigoCuentaCorriente;
+    }
+
+    public int getCodigoSucursal() {
+        return codigoSucursal;
+    }
+
+    public void setCodigoSucursal(int codigoSucursal) {
+        this.codigoSucursal = codigoSucursal;
+    }
+
+    public String getTipo() {
+        return tipo;
     }
 
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
 
-    public int getCodigoTarjeta() {
-        return codigoTarjeta;
-    }
-
-    public int getCodigoTitular() {
-        return codigoTitular;
-    }
-
-    public int getCodigoCuentaCorriente() {
-        return codigoCuentaCorriente;
-    }
-
-    public void setCodigoTitular(int codigoTitular) {
-        this.codigoTitular = codigoTitular;
-    }
-
-    public void setCodigoCuentaCorriente(int codigoCuentaCorriente) {
-        this.codigoCuentaCorriente = codigoCuentaCorriente;
-    }
-
-
-
-    public String getTipo() {
-        return tipo;
-    }
 }
