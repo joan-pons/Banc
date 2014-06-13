b2afe53c6606763b9f7e155925937b396f4dcba2
Roberto <Roberto@Roberto>
2014-06-13 20:12:27 +0200

    Modificación de los retornos de los métodos de CuentaCorriente más la implementación del JavaDoc.

--- Banco/src/es/bancodehierro/banco/cc/CuentaCorriente.java
+++ Banco/src/es/bancodehierro/banco/cc/CuentaCorriente.java
@@ -91,30 +91,48 @@ public class CuentaCorriente {
         this.importe = importe;
     }
 
+    /**
+     * Muestra la cuenta corriente.
+     * 
+     * @return Un string con la cadena de la cuenta corriente. 
+     */
     public String muestraCC() {
         return (iban + ENTIDAD + oficina + dC + cuenta);
     }
 
-    public void agregarTitular(Cliente titular, Sucursal sucursal) throws CuentaCorrienteException, SQLException {
+    /**
+     * Agregación de un titular en la cuenta corriente. Si no hay ningún titular
+     * lo agregará como titular. En el HashMap de CuentaCorriente y en la base
+     * de datos. Si hay un titular asociado a la cuenta, agregará el nuevo como
+     * segundo titular. En la HashMap de CuentaCorriente y en la base de datos.
+     *
+     * @param titular El cliente que se desea asociar a la cuenta corriente.
+     * @param sucursal La sucursal a la que pertenece la cuenta corriente.
+     * @return El número de filas afectadas.
+     * @throws CuentaCorrienteException En caso que ya existean dos clientes en
+     * la cuenta corriente o ya exista el cliente nuevo en la cuenta correinte.
+     * @throws SQLException En el caso que haya fallado alguna sentencia a la base de datos.
+     */
+    public int agregarTitular(Cliente titular, Sucursal sucursal) throws CuentaCorrienteException, SQLException {
+        int filas = '\0';
         try (Statement st = Conexion.conectar().createStatement()) {
             String consultaTitular = "SELECT * FROM CLIENTE WHERE DNI_CLIENTE='" + titular.getDni() + "'";
             if (st.executeQuery(consultaTitular) != null) {
                 String insertTitular = null;
-                int filas = '\0';
                 String consultaCountTitular = "SELECT count(*) FROM CLIENTE_CUENTA_CORRIENTE WHERE NUMERO_CC='" + muestraCC() + "' AND CODIGO_SUCURSAL=" + sucursal.getCodi();
                 ResultSet rs = st.executeQuery(consultaCountTitular);
                 rs.next();
                 if (rs.getInt(1) == 0) {
                     insertTitular = "INSERT INTO CLIENTE_CUENTA_CORRIENTE (DNI_CLIENTE_CC,NUMERO_CC,CODIGO_SUCURSAL,FECHA_CREACION,POSICIO) VALUES('" + titular.getDni() + "','" + muestraCC() + "','" + sucursal.getCodi() + "'," + "SYSTIMESTAMP" + ",1";
-                    
+
                 } else if (rs.getInt(1) == 1) {
                     insertTitular = "INSERT INTO CLIENTE_CUENTA_CORRIENTE (DNI_CLIENTE_CC,NUMERO_CC,CODIGO_SUCURSAL,FECHA_CREACION,POSICIO) VALUES('" + titular.getDni() + "','" + muestraCC() + "','" + sucursal.getCodi() + "'," + "SYSTIMESTAMP" + ",2";
-                    
+
                 } else {
-                    System.err.println("Ya tiene dos titulares, no puedes añadir más");
+                    throw new CuentaCorrienteException("Ya tiene dos titulares, no puedes añadir más");
                 }
                 filas = st.executeUpdate(insertTitular);
-                System.out.println("Filas insertadas: " + filas);
+                
             }
         }
         if (!titulares.containsValue(titular)) {
@@ -128,29 +146,42 @@ public class CuentaCorriente {
         } else {
             throw new CuentaCorrienteException("Error: Titular, " + titular.getDni() + ", ya asociado a está cuenta.");
         }
+        
+        return filas;
     }
 
-    public void eliminarTitular(Cliente cliente, Sucursal sucursal) throws CuentaCorrienteException, SQLException {
+    /**
+     * Eliminación de un cliente de la cuenta corriente. Si se elimina el
+     * titular y existe un segundo titular, el segundo cliente pasará a ser el
+     * titular de la cuenta corriente.
+     *
+     * @param cliente El cliente que se desea eliminar de la cuenta corriente.
+     * @param sucursal La sucursal a la que pertenece la cuenta corriente.
+     * @return El número de filas afectadas.
+     * @throws CuentaCorrienteException
+     * @throws SQLException  En el caso que haya fallado alguna sentencia a la base de datos.
+     */
+    public int eliminarTitular(Cliente cliente, Sucursal sucursal) throws CuentaCorrienteException, SQLException {
+        int filas = '\0';
         try (Statement st = Conexion.conectar().createStatement()) {
             String consultaTitular = "SELECT * FROM CLIENTE WHERE DNI_CLIENTE='" + cliente.getDni() + "'";
             if (st.executeQuery(consultaTitular) != null) {
                 String insertTitular = null;
                 String borrarTitular = null;
-                int filas = '\0';
                 String consultaCountTitular = "SELECT count(*) FROM CLIENTE_CUENTA_CORRIENTE WHERE NUMERO_CC='" + muestraCC() + "' AND CODIGO_SUCURSAL=" + sucursal.getCodi();
                 ResultSet rs = st.executeQuery(consultaCountTitular);
                 rs.next();
                 if (rs.getInt(1) == 1) {
                     System.err.println("No tienes mas titulares, antes da de baja la cuenta");
                     //insertTitular = "INSERT INTO CLIENTE_CUENTA_CORRIENTE (DNI_CLIENTE_CC,NUMERO_CC,CODIGO_SUCURSAL,FECHA_CREACION,POSICIO) VALUES('" + cliente.getDni() + "','" + muestraCC() + "','" + sucursal.getCodi() + "'," + "SYSTIMESTAMP" + ",2";
-                    
+
                 } else if (rs.getInt(1) > 1) {
                     String consultaPosicionTitular = "SELECT POSICIO FROM CLIENTE_CUENTA_CORRIENTE WHERE DNI_CLIENTE_CC='" + cliente.getDni();
                     rs = st.executeQuery(consultaPosicionTitular);
                     rs.next();
                     if (rs.getInt(1) == 1) {
                         intercambiarTitular(sucursal);
-                        
+
                     }
                     borrarTitular = "DELETE FROM CLIENTE_CUENTA_CORRIENTE WHERE POSICIO= 2";
                 }
@@ -173,71 +204,75 @@ public class CuentaCorriente {
         } else {
             throw new CuentaCorrienteException("Error: Ya existe en esta cuenta.");
         }
+        
+        return filas;
     }
 
-    public void intercambiarTitular(Sucursal sucursal) throws CuentaCorrienteException, SQLException {
+    /**
+     * Intercambio de titular a segundo titular y viceversa.
+     * 
+     * @param sucursal La sucursal a la que pertenece la cuenta corriente.
+     * @return El número de filas afectadas.
+     * @throws CuentaCorrienteException En caso que solo haya un cliente asociado en la cuenta corriente.
+     * @throws SQLException  En el caso que haya fallado alguna sentencia a la base de datos.
+     */
+    public int intercambiarTitular(Sucursal sucursal) throws CuentaCorrienteException, SQLException {
+        int filas = '\0';
         try (Statement st = Conexion.conectar().createStatement()) {
-            
-                String insertTitular = null;
-                int filas = '\0';
-                String consultaCountTitular = "SELECT count(*) FROM CLIENTE_CUENTA_CORRIENTE WHERE NUMERO_CC='" + muestraCC() + "' AND CODIGO_SUCURSAL=" + sucursal.getCodi();
-                ResultSet rs = st.executeQuery(consultaCountTitular);
-                rs.next();
-                if (rs.getInt(1) == 0) {
-                    System.err.println("ERROR");
-                } else if (rs.getInt(1) == 1) {
-                    System.err.println("ERROR: solo hay un titular");
-                } /*else {
-                     insertTitular = "UPDATE CLIENTE_CUENTA_CORRIENTE SET POSICIO= 3 WHERE;
+            String insertTitular = null;
+            String consultaCountTitular = "SELECT count(*) FROM CLIENTE_CUENTA_CORRIENTE WHERE NUMERO_CC='" + muestraCC() + "' AND CODIGO_SUCURSAL=" + sucursal.getCodi();
+            ResultSet rs = st.executeQuery(consultaCountTitular);
+            rs.next();
+            if (rs.getInt(1) == 0) {
+                throw new CuentaCorrienteException("ERROR");
+            } else if (rs.getInt(1) == 1) {
+                throw new CuentaCorrienteException("ERROR: solo hay un titular");
+            } /*else {
+             insertTitular = "UPDATE CLIENTE_CUENTA_CORRIENTE SET POSICIO= 3 WHERE;
                     
-                }*/
-                filas = st.executeUpdate(insertTitular);
-                System.out.println("Filas insertadas: " + filas);
-            }
-        
-        
-        
-        Cliente auxiliar = titulares.get("Titular");
-
-        eliminarTitular(titulares.get("Titular"),sucursal);
-
-        try {
-            agregarTitular(auxiliar, sucursal);
-        } catch (SQLException ex) {
-            System.err.println(ex.getMessage());
+             }*/
+            filas = st.executeUpdate(insertTitular);
         }
 
+        Cliente auxiliar = titulares.get("Titular");
+        eliminarTitular(titulares.get("Titular"), sucursal);
+        agregarTitular(auxiliar, sucursal);
+
+        return filas;
     }
 
+    /**
+     * Cambio de titulares de la cuenta corriente.
+     * 
+     * @param viejo El cliente que se desea desvincular.
+     * @param nuevo El cliente que se desea vincular a la cuenta corriente.
+     * @param sucursal La sucursal a la que pertenece la cuenta corriente.
+     * @throws CuentaCorrienteException 
+     * @throws SQLException En el caso que haya fallado alguna sentencia a la base de datos.
+     */
     public void cambiarTitular(Cliente viejo, Cliente nuevo, Sucursal sucursal) throws CuentaCorrienteException, SQLException {
-
         if (!titulares.containsKey("Segundo")) {
-            try {
-                agregarTitular(nuevo, sucursal);
-            } catch (SQLException ex) {
-                System.err.println(ex.getMessage());
-            }
-            eliminarTitular(viejo,sucursal);
+            agregarTitular(nuevo, sucursal);
+            eliminarTitular(viejo, sucursal);
         } else {
             if (viejo.equals(titulares.get("Titular"))) {
-                eliminarTitular(viejo,sucursal);
-                try {
-                    agregarTitular(nuevo, sucursal);
-                } catch (SQLException ex) {
-                    System.err.println(ex.getMessage());
-                }
+                eliminarTitular(viejo, sucursal);
+                agregarTitular(nuevo, sucursal);
                 intercambiarTitular(sucursal);
             } else {
-                eliminarTitular(viejo,sucursal);
-                try {
-                    agregarTitular(nuevo, sucursal);
-                } catch (SQLException ex) {
-                    System.err.println(ex.getMessage());
-                }
+                eliminarTitular(viejo, sucursal);
+                agregarTitular(nuevo, sucursal);
             }
         }
     }
 
+    /**
+     * Devuelve los movimientos de la cuenta corriente.
+     * 
+     * @param incidencia true en el caso que quieran las incidencias, false si quieren los movimientos.
+     * @return Un ArrayList con los movimientos filtrados.
+     * @throws CuentaCorrienteException En el caso que no haya movimientos.
+     */
     public ArrayList<Movimiento> mostrarMovimiento(Boolean incidencia) throws CuentaCorrienteException {
         if (incidencia == true) {
             if (incidencias.isEmpty()) {
@@ -254,6 +289,14 @@ public class CuentaCorriente {
         }
     }
 
+    /**
+     * Muestra todos los movimientos o las incidencias de la cuenta corriente filtradas por el tipo de movimiento.
+     * 
+     * @param incidencia true en el caso que quieran las incidencias, false si quieren los movimientos.
+     * @param tipo El tipo de movimiento que desean.
+     * @return Un ArrayList con los movimientos filtrados.
+     * @throws CuentaCorrienteException En el caso que no haya movimientos.
+     */
     public ArrayList<Movimiento> mostrarMovimiento(Boolean incidencia, EnumMovimiento tipo) throws CuentaCorrienteException {
         ArrayList<Movimiento> movimientoFiltrado = new ArrayList<>();
         if (incidencia == true) {
@@ -290,6 +333,5 @@ public class CuentaCorriente {
     public String toString() {
         return "CuentaCorriente{" + "cuenta=" + muestraCC() + ", importe=" + importe + '}';
     }
-    
-    
+
 }
