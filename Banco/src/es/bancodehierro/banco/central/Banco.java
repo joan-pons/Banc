60ac6c41a93c8eedc86ee3f78a46ee15a89f2e23
Miguel Vallespir Castello <vallespirmiquel@gmail.com>
2014-06-13 19:06:52 +0200

    Insertar Prestamo
    Insertar prestamo insertat

--- Banco/src/es/bancodehierro/banco/central/Banco.java
+++ Banco/src/es/bancodehierro/banco/central/Banco.java
@@ -152,4 +152,59 @@ public class Banco {
     }
 
     //Fi de la part del grup de Guillem Arrom, Rotger, Pedro i François
+
+    /**
+     * MÉTODO INSERTAR PRESTAMO - PENDIENTE DE PASAR A BANCO.
+     *
+     * @author Rafel Sastre, Miquel Vallespir i Pau Riera.
+     * @param presta
+     */
+    public void insertarPrestamo(Empleado empleado) throws ClienteException, CuentaCorrienteException {
+        Connection conexion = Conexion.conectar();
+        ArrayList<CuentaCorriente> listCC = null;
+        Banco b = new Banco();// al meter en banco desaparece
+
+        String dniCliente = GestionaMenu.llegirCadena("Introduce DNI cliente.");
+
+        Cliente cliente = b.devuelveCliente(dniCliente);
+
+        if (cliente == null) {
+            throw new ClienteException("El cliente con +"+dniCliente+" no ha sido encontrado.");
+            //return false;
+        } else {
+            listCC = b.mostrarCuentaCorriente(cliente);
+        }
+
+        if (listCC == null) {
+            throw new CuentaCorrienteException("Este cliente no tiene ninguna cuenta asociada.");
+            //return false;
+        }
+
+        String[] opcions = new String[listCC.size()];
+        int i = 0;
+        for (CuentaCorriente cc : listCC) {
+
+            opcions[i] = cc.muestraCC();
+            i++;
+
+        }
+
+        int opcioSeleccionada = GestionaMenu.gestionarMenu("Prestamo", opcions, "Insertar opcion:", 0);
+        double importePrestado = GestionaMenu.llegirDouble("Introdueix el total a prestar");
+        String fechaInicio = GestionaMenu.llegirCadena("Introdueix fecha inicial (dd-mm-yyyy)");
+        String fechaFinal = GestionaMenu.llegirCadena("Introdueix fecha final (dd-mm-yyyy)");
+
+        //Prestamo presta = new Prestamo(0, "", null, null, importePrestado, tasaInteresAnual, empleado, listCC.get(opcioSeleccionade));
+        Prestamo presta = new Prestamo(0, importePrestado, fechaInicio, empleado, fechaFinal, null, listCC.get(opcioSeleccionada));
+        try {
+            Statement st = conexion.createStatement();
+            int filesAfectades = st.executeUpdate(presta.insertarPrestamo());
+            System.out.println(filesAfectades + ", files afectades.");
+        } catch (SQLException ex) {
+            System.out.println("ERROR: " + ex.getErrorCode() + ", " + ex.getLocalizedMessage());
+        }
+        presta.toString();
+
+    }
+
 }
