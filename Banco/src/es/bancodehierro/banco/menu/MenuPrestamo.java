81c7a2e3490f29f4bc62c94b40b4a6a7f3545f06
mvallespir <vallespirmiquel@gmail.com>
2014-06-16 15:44:45 +0200

    Javadoc Menu Prestamo

--- Banco/src/es/bancodehierro/banco/menu/MenuPrestamo.java
+++ Banco/src/es/bancodehierro/banco/menu/MenuPrestamo.java
@@ -1,27 +1,25 @@
 package es.bancodehierro.banco.menu;
 
-import es.bancodehierro.banco.cc.CuentaCorriente;
 import es.bancodehierro.banco.central.Banco;
-import es.bancodehierro.banco.conexion.Conexion;
 import es.bancodehierro.banco.excepciones.ClienteException;
 import es.bancodehierro.banco.excepciones.CuentaCorrienteException;
-import es.bancodehierro.banco.persona.Cliente;
 import es.bancodehierro.banco.persona.Empleado;
-import es.bancodehierro.banco.prestamo.Prestamo;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
+ * SUB MENU DEL APARTADO PRESTAMO. Se muestran las opciones de este sub-menu y
+ * te pide que introduxcas la opcion. Segun la opcion elegida ejecuta el metodo
+ * de banco encargado de pedir los datos.
  *
  * @author Miquel Vallespir, Rafel Sastre, Pau Riera, Jaume Mayol, Tomeu Moranta
+ *
  */
 public class MenuPrestamo {
 
+    /**
+     * Opcion del menu
+     *
+     * @param empleado Se pasa el objeto del empleado que hara las opciones.
+     */
     public static void menuPres(Empleado empleado) {
 
         String[] opcions = {"Insertar Prestamo", "Eliminar Prestamo", "Atras"};
